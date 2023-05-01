a109e74864ab2108201c567cb8db21bc7d4f48e6
Rita Gujar <75436627+ritagujar@users.noreply.github.com>
2023-05-01 21:54:32 +0530

    Time: 25 ms (38.45%), Space: 43.6 MB (25.24%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+           
+        s=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        
+        int low = 0;
+        int high = s.length()-1;
+        
+        while(low < high){
+            if(s.charAt(low++) != s.charAt(high--))
+                return false;
+        }
+        return true;
+    }
+}
