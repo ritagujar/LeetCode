e62b53487d391408c826415104885fcfa8208556
Rita Gujar <75436627+ritagujar@users.noreply.github.com>
2023-05-03 22:39:58 +0530

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.java
+++ Selection Sort - GFG/selection-sort.java
@@ -43,13 +43,14 @@ class Solution
 	{
 	    //code here
 	    for(int i = 0; i < n-1; i++){
-	        for(int j = i + 1; j < n; j++) {
-	            if(arr[i] > arr[j]) {
+	        for(int j = i + 1; j < n; j++){
+	            if(arr[i] > arr[j]){
 	                int temp = arr[i];
 	                arr[i] = arr[j];
 	                arr[j] = temp;
 	            }
 	        }
 	    }
+	    
 	}
 }
