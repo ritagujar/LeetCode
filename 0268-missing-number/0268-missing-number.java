a303a2af9421bb83667e3378e4ef160ddaa72dfa
Rita Gujar <75436627+ritagujar@users.noreply.github.com>
2023-05-07 21:17:45 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (7.17%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum1 = 0;
+        
+        int sum2 = (n*(n+1))/2;
+        
+        for(int num: nums)
+                sum1 += num;
+        
+        return Math.abs(sum2 - sum1);
+    }
+}
