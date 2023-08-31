4fa2a6b83559de0391d9153748f17dfc274ee32b
Rita Gujar <75436627+ritagujar@users.noreply.github.com>
2023-08-31 21:55:33 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (99.92%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -7,13 +7,13 @@ class Solution {
         for(String sentence: sentences){
             char ch = sentence.charAt(sentence.length()-1);
             int num = ch - '0';
-            //System.out.println(num);
             res[num-1] = sentence.substring(0, sentence.length()-1);
         }
         
-        for(String r: res)
-            result += r + " ";
-        System.out.println(Arrays.toString(res));
-        return result.substring(0, result.length()-1);
+        // for(String r: res)
+        //     result += r + " ";
+      
+        // return result.substring(0, result.length()-1);
+        return String.join(" ", res);
     }
 }
